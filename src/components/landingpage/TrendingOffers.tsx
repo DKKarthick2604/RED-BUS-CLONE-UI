a081483fa603594114caa7ee97f3e6f28a0485e3
Ahimsa <ahimsamoorthy@gove.co>
2023-07-18 00:07:30 +0530

    Completed Landing Page

--- src/components/landingpage/TrendingOffers.tsx
+++ src/components/landingpage/TrendingOffers.tsx
@@ -1,21 +1,71 @@
-import { TrendingOffersConfig } from "@/configs/landingpage/TrendingOffersConfig"
-import { Card } from "@mui/material"
-import Carousel from 'react-material-ui-carousel'
+import React from 'react';
+import { TrendingOffersConfig } from "@/configs/landingpage/TrendingOffersConfig";
+import { Box, Card, Typography, Grid, Chip } from "@mui/material";
+import Carousel from 'react-material-ui-carousel';
+import Image from 'next/image';
 
-const trendingOffersConfig = new TrendingOffersConfig()
+const trendingOffersConfig = new TrendingOffersConfig();
 
 export const TrendingOffers = () => {
-    return (
-        <>
-            <Carousel>
+    const items = trendingOffersConfig?.TRENDING_OFFERS_CONFIG;
+
+    const renderSlides = () => {
+        const slides = [];
+        let startIndex = 0;
+
+        while (startIndex < items.length) {
+            const slideItems = items.slice(startIndex, startIndex + 3);
+            slides.push(renderSlideItems(slideItems));
+            startIndex += 3;
+        }
+
+        return slides;
+    };
+
+    const renderSlideItems = (slideItems: any) => {
+        return (
+            <Grid container spacing={2}>
                 {
-                    trendingOffersConfig?.TRENDING_OFFERS_CONFIG?.map((data: any) =>
-                        <Card sx={{ minWidth: 275 }}>
-                            Test {data?.title}
-                        </Card>
-                    )
+                    slideItems.map((data: any) => (
+                        <Grid item xs={4}>
+                            <Card
+                                key={data?.title}
+                                sx={{
+                                    backgroundImage: data?.color,
+                                    padding: '15px',
+                                    margin: '10px',
+                                    borderRadius: '20px',
+                                    boxShadow: '1px 0px 5px 0px rgba(0,0,0,0.25)',
+                                    color: 'white'
+                                }}
+                            >
+                                <Grid container spacing={2}>
+                                    <Grid item xs={3} sx={{ display: 'flex', alignItems: 'center' }}>
+                                        <Image src={data?.imageURL} height={50} width={50} alt='card-images' />
+                                    </Grid>
+                                    <Grid item xs={9}>
+                                        <Chip sx={{ color: 'white', fontSize: '12px', paddingBottomm: '10px' }} size='small' label={data?.type} variant="outlined" /><br />
+                                        <Typography variant='caption' sx={{ fontSize: '14px', fontWeight: 'bold' }}>{data?.title}</Typography><br />
+                                        <Typography variant='caption'>{data?.validity}</Typography> <br />
+                                        <Typography variant='caption' sx={{ fontSize: '12px', fontWeight: 'bold', border: '2px dotted', padding: '3px' }}>{data?.offerCode}</Typography>
+                                    </Grid>
+                                </Grid>
+                            </Card>
+                        </Grid>
+                    ))
                 }
-            </Carousel>
-        </>
-    )
-}
+            </Grid>
+        )
+    };
+
+    return (
+        <Box sx={{ paddingLeft: '200px', paddingRight: '200px', marginTop: '-50px' }}>
+            <Card sx={{ padding: '20px', borderRadius: '20px', boxShadow: '1px 0px 5px 0px rgba(0,0,0,0.25)' }}>
+                <Typography variant="h4">TRENDING OFFERS</Typography>
+                <Carousel>
+                    {renderSlides()}
+                </Carousel>
+            </Card>
+        </Box>
+    );
+};
--- src/configs/landingpage/TrendingOffersConfig.ts
+++ src/configs/landingpage/TrendingOffersConfig.ts
@@ -5,28 +5,63 @@ export class TrendingOffersConfig {
             type: 'BUS',
             validity: 'Valid till 11 Jul',
             offerCode: 'FIRST',
-            imageURL: 'https://st.redbus.in/images/FIRST/first_26th_sep_2022_ravi/tile-80X80.png'
+            imageURL: 'https://st.redbus.in/images/FIRST/first_26th_sep_2022_ravi/tile-80X80.png',
+            color: 'linear-gradient(71.04deg, rgb(12, 57, 139) -6.92%, rgb(62, 118, 216) 109.43%)'
         },
         {
             title: "Save up to Rs 350 on bus tickets",
             type: 'BUS',
             validity: 'Valid till 31 Jul',
             offerCode: 'SUPER HIT',
-            imageURL: 'https://st.redbus.in/images/FIRST/first_26th_sep_2022_ravi/tile-80X80.png'
+            imageURL: 'https://st.redbus.in/images/offers/superhit_rav/2_1.png',
+            color: 'linear-gradient(71.04deg, rgb(39, 57, 101) -6.92%, rgb(80, 210, 149) 109.43%)'
         },
         {
             title: "Save up to Rs 700 on bus tickets",
             type: 'BUS',
             validity: 'Valid till 31 Aug',
             offerCode: 'BUS 200',
-            imageURL: 'https://st.redbus.in/images/FIRST/first_26th_sep_2022_ravi/tile-80X80.png'
+            imageURL: 'https://st.redbus.in/Images/INDOFFER/BUS200/80x80.png',
+            color: 'linear-gradient(71.04deg, rgb(72, 0, 4) -6.92%, rgb(216, 78, 85) 109.43%)'
         },
         {
             title: "Save up to Rs 840 on bus tickets",
             type: 'BUS',
             validity: 'Valid till 31 Jul',
             offerCode: 'RB 200',
-            imageURL: 'https://st.redbus.in/images/FIRST/first_26th_sep_2022_ravi/tile-80X80.png'
+            imageURL: 'https://st.redbus.in/Images/APSRTC/new/APSRTC_3.png',
+            color: 'linear-gradient(71.04deg, rgb(14, 113, 67) -6.92%, rgb(115, 236, 179) 109.43%)'
+        }, {
+            title: "Save up to Rs 250 on bus tickets",
+            type: 'BUS',
+            validity: 'Valid till 11 Jul',
+            offerCode: 'FIRST',
+            imageURL: '	https://st.redbus.in/Images/TSRTC/80x80.png',
+            color: 'linear-gradient(71.04deg, rgb(12, 57, 139) -6.92%, rgb(62, 118, 216) 109.43%)'
+        },
+        {
+            title: "Save up to Rs 350 on bus tickets",
+            type: 'BUS',
+            validity: 'Valid till 31 Jul',
+            offerCode: 'SUPER HIT',
+            imageURL: 'https://st.redbus.in/Images/INDOFFER/BUS200/80x80.png',
+            color: 'linear-gradient(71.04deg, rgb(39, 57, 101) -6.92%, rgb(80, 210, 149) 109.43%)'
+        },
+        {
+            title: "Save up to Rs 700 on bus tickets",
+            type: 'BUS',
+            validity: 'Valid till 31 Aug',
+            offerCode: 'BUS 200',
+            imageURL: 'https://st.redbus.in/images/FIRST/first_26th_sep_2022_ravi/tile-80X80.png',
+            color: 'linear-gradient(71.04deg, rgb(72, 0, 4) -6.92%, rgb(216, 78, 85) 109.43%)'
+        },
+        {
+            title: "Save up to Rs 840 on bus tickets",
+            type: 'BUS',
+            validity: 'Valid till 31 Jul',
+            offerCode: 'RB 200',
+            imageURL: 'https://st.redbus.in/Images/TSRTC/80x80.png',
+            color: 'linear-gradient(71.04deg, rgb(14, 113, 67) -6.92%, rgb(115, 236, 179) 109.43%)'
         }
     ]
 } 
